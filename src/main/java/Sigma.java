cd7c672bb0da1e419c7eb07f34a29ede02f0c088
Zhang-Jiaxiang0923 <e1485166@u.nus.edu>
2026-01-22 10:50:21 +0800

    add Echo

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -1,10 +1,6 @@
+import java.util.Scanner;
 public class Sigma {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
         String logo =
                 " ____   ___    ____   __  __     _    \n"
                         + "/ ___| |_ _|  / ___| |  \\/  |   / \\   \n"
@@ -12,10 +8,24 @@ public class Sigma {
                         + " ___) | | |  | |_| | | |  | | / ___ \\ \n"
                         + "|____/ |___|  \\____| |_|  |_|/_/   \\_\\\n";
         String name = "Sigma";
+        int width = 60;
+        String indentation = "    ";
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
 
+        System.out.println("_".repeat(width));
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_".repeat(width));
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("Bye") || s.equals("bye")) break;
+            System.out.println(indentation + "_".repeat(width-4));
+            System.out.println(indentation + s);
+            System.out.println(indentation + "_".repeat(width-4));
+        }
+        System.out.println(indentation + "_".repeat(width-4));
+        System.out.println(indentation + "Bye. Hope to see you again soon!");
+        System.out.println(indentation + "_".repeat(width-4));
     }
 }
