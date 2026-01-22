5cea723b54a113a8ecd9a2fe64e3d3dc16ecd12e
Zhang-Jiaxiang0923 <e1485166@u.nus.edu>
2026-01-22 10:21:48 +0800

    rename and add greeting

---
+++ src/main/java/Sigma.java
@@ -0,0 +1,21 @@
+public class Sigma {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo =
+                " ____   ___    ____   __  __     _    \n"
+                        + "/ ___| |_ _|  / ___| |  \\/  |   / \\   \n"
+                        + "\\___ \\  | |  | |  _  | |\\/| |  / _ \\  \n"
+                        + " ___) | | |  | |_| | | |  | | / ___ \\ \n"
+                        + "|____/ |___|  \\____| |_|  |_|/_/   \\_\\\n";
+        String name = "Sigma";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
