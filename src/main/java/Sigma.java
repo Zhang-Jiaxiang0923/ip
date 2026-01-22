88d22471595bc963a6faffa4e0008cca70327a96
Zhang-Jiaxiang0923 <e1485166@u.nus.edu>
2026-01-22 11:12:09 +0800

    enable add and list

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Sigma {
     public static void main(String[] args) {
@@ -13,19 +15,33 @@ public class Sigma {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("_".repeat(width));
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("_".repeat(width));
+        List<String> todo = new ArrayList<>();
+
+        System.out.println(indentation + "_".repeat(width));
+        System.out.println(indentation + "Hello! I'm " + name);
+        System.out.println(indentation + "What can I do for you?");
+        System.out.println(indentation + "_".repeat(width));
         while (true) {
             String s = sc.nextLine();
+            System.out.println(indentation + "_".repeat(width));
             if (s.equals("Bye") || s.equals("bye")) break;
-            System.out.println(indentation + "_".repeat(width-4));
-            System.out.println(indentation + s);
-            System.out.println(indentation + "_".repeat(width-4));
+            if (s.equals("list") || s.equals("List")) {
+                Integer num = 1;
+                if (todo.isEmpty()){
+                    System.out.println(indentation + "Todo list is empty :)");
+                } else {
+                    for (String task : todo) {
+                        System.out.println(indentation + String.valueOf(num) + ". " + task);
+                        num++;
+                    }
+                }
+            } else {
+                System.out.println(indentation + "added: " + s);
+                todo.add(s);
+            }
+            System.out.println(indentation + "_".repeat(width));
         }
-        System.out.println(indentation + "_".repeat(width-4));
         System.out.println(indentation + "Bye. Hope to see you again soon!");
-        System.out.println(indentation + "_".repeat(width-4));
+        System.out.println(indentation + "_".repeat(width));
     }
 }
