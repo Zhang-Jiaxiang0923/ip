9de0ad8b2f0767412f7d32fa0f9a8a862449288d
Zhang-Jiaxiang0923 <e1485166@u.nus.edu>
2026-01-22 12:22:26 +0800

    add mark functionality

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -15,29 +15,57 @@ public class Sigma {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello from\n" + logo);
 
-        List<String> todo = new ArrayList<>();
+        List<Task> todo = new ArrayList<Task>();
 
         System.out.println(indentation + "_".repeat(width));
         System.out.println(indentation + "Hello! I'm " + name);
         System.out.println(indentation + "What can I do for you?");
         System.out.println(indentation + "_".repeat(width));
+        Loop:
         while (true) {
-            String s = sc.nextLine();
+            String line = sc.nextLine();
+            String[] parts = line.trim().split("\\s+");
+            String s = parts[0];
+//            Integer index = Integer.valueOf(parts[1]);
             System.out.println(indentation + "_".repeat(width));
-            if (s.equals("Bye") || s.equals("bye")) break;
-            if (s.equals("list") || s.equals("List")) {
-                Integer num = 1;
-                if (todo.isEmpty()){
-                    System.out.println(indentation + "Todo list is empty :)");
-                } else {
-                    for (String task : todo) {
-                        System.out.println(indentation + String.valueOf(num) + ". " + task);
-                        num++;
-                    }
+            switch (s) {
+                case "bye": {
+                    break Loop;
                 }
-            } else {
-                System.out.println(indentation + "added: " + s);
-                todo.add(s);
+                case "list": {
+                    Integer num = 1;
+                    if (todo.isEmpty()) {
+                        System.out.println(indentation + "Todo list is empty :)");
+                    } else {
+                        System.out.println(indentation + "Here are the tasks in your list:");
+                        for (Task task : todo) {
+                            System.out.println(indentation + String.valueOf(num) + "." + task.getFullDescription());
+                            num++;
+                        }
+                    }
+                    break;
+                }
+                case "mark": {
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    Task task = todo.get(index);
+                    task.markAsDone();
+                    System.out.println(indentation + "Nice! I've marked this task as done:");
+                    System.out.println(indentation + "  " + task.getFullDescription());
+                    break;
+                }
+                case "unmark": {
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    Task task = todo.get(index);
+                    task.unmarkDone();
+                    System.out.println(indentation + "Ok, I've marked this task as not done yet:");
+                    System.out.println(indentation + "  " + task.getFullDescription());
+                    break;
+                }
+                default: {
+                    System.out.println(indentation + "added: " + line);
+                    Task task = new Task(line);
+                    todo.add(task);
+                }
             }
             System.out.println(indentation + "_".repeat(width));
         }
